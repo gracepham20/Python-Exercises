7474a834c9a1019ccf88442edc6ab14c94f99d8a
Grace Pham <gracepham20@gmail.com>
2020-10-02 13:25:04 +0700

    Update file

--- Problem4/StorefrontConfig.py
+++ Problem4/StorefrontConfig.py
@@ -3,11 +3,11 @@ class StorefrontConfig:
         self.data = data
 
     def modify(self, d, md):
-        for key in d.keys():
-            if key in md.keys():
+        for key in md.keys():
+            if key in d.keys():
                 if isinstance(d.get(key), list):
                     d[key] = md[key]
-                elif isinstance(d.get(key), dict):
+                elif isinstance(d.get(key), dict) and isinstance(md.get(key), dict):
                     self.modify(d[key], md[key])
                 else:
                     pass
