608af387a445c59f46d6da88be40f684a031394b
Grace Pham <gracepham20@gmail.com>
2020-10-06 13:50:15 +0700

    feat: Upload input file for excelchat.py

---
+++ Selenium/fileinput.py
@@ -0,0 +1,21 @@
+class FileInput:
+
+    baseUrl = "https://www.got-it.io/solutions/excel-chat/"
+    login_email = "grace+ec3@gotitapp.co"
+    login_pass = "Grace123"
+
+    card_no = "5555555555554444"
+    card_no_iframe = "braintree-hosted-field-number"
+    card_no_CSS = "input[name='credit-card-number']"
+
+    expiration_date = "08/22"
+    expiration_date_iframe = "braintree-hosted-field-expirationDate"
+    expiration_date_CSS = "input[name='expiration']"
+
+    cvv = "123"
+    cvv_iframe = "braintree-hosted-field-cvv"
+    cvv_CSS = "input[name='cvv']"
+
+    postal = "100000"
+    postal_iframe = "braintree-hosted-field-postalCode"
+    postal_CSS = "#postal-code"
